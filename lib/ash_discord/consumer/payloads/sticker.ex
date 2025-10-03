00d8b17c0445afa706708c2552a3db3c7cce70df
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2025-10-04 00:46:24 +0200

    feat: expand Sticker TypedStruct with all Nostrum fields
    Add all 11 fields from Nostrum.Struct.Sticker with proper Ash types
    and custom new/1 function to convert from Nostrum structs.

--- lib/ash_discord/consumer/payloads/sticker.ex
+++ lib/ash_discord/consumer/payloads/sticker.ex
@@ -2,14 +2,37 @@ defmodule AshDiscord.Consumer.Payloads.Sticker do
   @moduledoc """
   TypedStruct wrapper for Discord Sticker data.
 
-  Wraps `Nostrum.Struct.Sticker.t()` to provide a unified AshDiscord type.
+  Provides a unified AshDiscord type with all fields from `Nostrum.Struct.Sticker.t()`.
   """
 
   use Ash.TypedStruct
 
   typed_struct do
-    field :data, :map,
-      allow_nil?: false,
-      description: "The Nostrum.Struct.Sticker struct"
+    field :id, :integer, allow_nil?: false, description: "ID of the sticker"
+    field :pack_id, :integer, description: "ID of the pack the sticker is from"
+    field :name, :string, description: "Name of the sticker"
+    field :description, :string, description: "Description of the sticker"
+
+    field :tags, :string,
+      description: "Autocomplete/suggestion tags for the sticker (comma-separated)"
+
+    field :type, :integer, description: "Type of sticker (1 = standard, 2 = guild)"
+
+    field :format_type, :integer,
+      description: "Format type (1 = png, 2 = apng, 3 = lottie, 4 = gif)"
+
+    field :available, :boolean, description: "Whether this guild sticker can be used"
+    field :guild_id, :integer, description: "ID of the guild that owns this sticker"
+    field :user, :map, description: "User that uploaded the guild sticker"
+    field :sort_value, :integer, description: "Standard sticker's sort order within its pack"
+  end
+
+  @doc """
+  Create a Sticker TypedStruct from a Nostrum Sticker struct.
+
+  Accepts a `Nostrum.Struct.Sticker.t()` and creates an AshDiscord Sticker TypedStruct.
+  """
+  def new(%Nostrum.Struct.Sticker{} = nostrum_sticker) do
+    super(Map.from_struct(nostrum_sticker))
   end
 end

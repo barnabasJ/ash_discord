0956fddd4b77537945da12dfcd038c900b54cddc
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2025-10-04 00:52:52 +0200

    feat: expand event TypedStructs with new/1 implementations (batch 1)
    - GuildBanAddEvent: guild_id, user (User TypedStruct)
    - GuildBanRemoveEvent: guild_id, user (User TypedStruct)
    - ChannelPinsUpdateEvent: guild_id, channel_id, last_pin_timestamp
    - GuildIntegrationDeleteEvent: id, guild_id, application_id

--- lib/ash_discord/consumer/payloads/guild_ban_add_event.ex
+++ lib/ash_discord/consumer/payloads/guild_ban_add_event.ex
@@ -2,14 +2,25 @@ defmodule AshDiscord.Consumer.Payloads.GuildBanAddEvent do
   @moduledoc """
   TypedStruct wrapper for Discord GUILD_BAN_ADD event data.
 
-  Wraps `Nostrum.Struct.Event.GuildBanAdd.t()` to provide a unified AshDiscord type.
+  Provides a unified AshDiscord type with all fields from `Nostrum.Struct.Event.GuildBanAdd.t()`.
   """
 
   use Ash.TypedStruct
 
   typed_struct do
-    field :data, :map,
+    field :guild_id, :integer, allow_nil?: false, description: "ID of the guild"
+
+    field :user, AshDiscord.Consumer.Payloads.User,
       allow_nil?: false,
-      description: "The Nostrum.Struct.Event.GuildBanAdd struct"
+      description: "Banned user"
+  end
+
+  @doc """
+  Create a GuildBanAddEvent TypedStruct from a Nostrum GuildBanAdd event struct.
+
+  Accepts a `Nostrum.Struct.Event.GuildBanAdd.t()` and creates an AshDiscord GuildBanAddEvent TypedStruct.
+  """
+  def new(%Nostrum.Struct.Event.GuildBanAdd{} = nostrum_event) do
+    super(Map.from_struct(nostrum_event))
   end
 end
--- lib/ash_discord/consumer/payloads/guild_ban_remove_event.ex
+++ lib/ash_discord/consumer/payloads/guild_ban_remove_event.ex
@@ -2,14 +2,25 @@ defmodule AshDiscord.Consumer.Payloads.GuildBanRemoveEvent do
   @moduledoc """
   TypedStruct wrapper for Discord GUILD_BAN_REMOVE event data.
 
-  Wraps `Nostrum.Struct.Event.GuildBanRemove.t()` to provide a unified AshDiscord type.
+  Provides a unified AshDiscord type with all fields from `Nostrum.Struct.Event.GuildBanRemove.t()`.
   """
 
   use Ash.TypedStruct
 
   typed_struct do
-    field :data, :map,
+    field :guild_id, :integer, allow_nil?: false, description: "ID of the guild"
+
+    field :user, AshDiscord.Consumer.Payloads.User,
       allow_nil?: false,
-      description: "The Nostrum.Struct.Event.GuildBanRemove struct"
+      description: "Unbanned user"
+  end
+
+  @doc """
+  Create a GuildBanRemoveEvent TypedStruct from a Nostrum GuildBanRemove event struct.
+
+  Accepts a `Nostrum.Struct.Event.GuildBanRemove.t()` and creates an AshDiscord GuildBanRemoveEvent TypedStruct.
+  """
+  def new(%Nostrum.Struct.Event.GuildBanRemove{} = nostrum_event) do
+    super(Map.from_struct(nostrum_event))
   end
 end

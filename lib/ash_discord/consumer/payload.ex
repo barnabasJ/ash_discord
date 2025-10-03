3b3f7362b99b0db1aefc4da7720e56227981e6af
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2025-10-03 22:03:39 +0200

    refactor: restructure Payload types following Nostrum's pattern
    Restructure AshDiscord.Consumer.Payload to follow Nostrum's pattern with
    individual named types for each event payload.
    
    Changes:
    - Define individual @type for each event (message_create, guild_update, etc.)
    - Each type is clearly named and documents the payload structure
    - Union type t() combines all individual named types
    - Follows same naming convention as Nostrum (snake_case event names)
    
    This provides better type documentation and makes it easy to reference
    specific payload types (e.g., Payload.message_create) throughout the
    codebase, while maintaining a comprehensive union type for generic use.

--- lib/ash_discord/consumer/payload.ex
+++ lib/ash_discord/consumer/payload.ex
@@ -2,100 +2,221 @@ defmodule AshDiscord.Consumer.Payload do
   @moduledoc """
   Type definitions for Discord event payloads.
 
-  This module consolidates all possible payload types from Nostrum events,
-  providing a unified type specification for use throughout AshDiscord.
+  This module defines individual named types for each Discord event payload,
+  following Nostrum's pattern. Each type represents the payload portion of
+  the event (without the event name atom and ws_state).
   """
 
   # Message events
+  @type message_create :: Nostrum.Struct.Message.t()
+
+  @type message_update ::
+          {old_message :: Nostrum.Struct.Message.t() | nil,
+           updated_message :: Nostrum.Struct.Message.t()}
+
+  @type message_delete :: Nostrum.Struct.Event.MessageDelete.t()
+
+  @type message_delete_bulk :: Nostrum.Struct.Event.MessageDeleteBulk.t()
+
+  # Interaction events
+  @type interaction_create :: Nostrum.Struct.Interaction.t()
+
+  # Guild events
+  @type guild_create :: Nostrum.Struct.Guild.t()
+
+  @type guild_update ::
+          {old_guild :: Nostrum.Struct.Guild.t(), new_guild :: Nostrum.Struct.Guild.t()}
+
+  @type guild_delete ::
+          {old_guild :: Nostrum.Struct.Guild.t(), unavailable :: boolean()}
+
+  @type guild_available :: Nostrum.Struct.Guild.t()
+
+  @type guild_unavailable :: Nostrum.Struct.Guild.t()
+
+  # Guild member events
+  @type guild_member_add ::
+          {guild_id :: integer(), member :: Nostrum.Struct.Guild.Member.t()}
+
+  @type guild_member_update ::
+          {guild_id :: integer(), old_member :: Nostrum.Struct.Guild.Member.t() | nil,
+           new_member :: Nostrum.Struct.Guild.Member.t()}
+
+  @type guild_member_remove ::
+          {guild_id :: integer(), user :: Nostrum.Struct.User.t()}
+
+  # Channel events
+  @type channel_create :: Nostrum.Struct.Channel.t()
+
+  @type channel_update ::
+          {old_channel :: Nostrum.Struct.Channel.t() | nil,
+           new_channel :: Nostrum.Struct.Channel.t()}
+
+  @type channel_delete :: Nostrum.Struct.Channel.t()
+
+  @type channel_pins_update :: Nostrum.Struct.Event.ChannelPinsUpdate.t()
+
+  # Role events
+  @type guild_role_create ::
+          {guild_id :: integer(), role :: Nostrum.Struct.Guild.Role.t()}
+
+  @type guild_role_update ::
+          {guild_id :: integer(), old_role :: Nostrum.Struct.Guild.Role.t() | nil,
+           new_role :: Nostrum.Struct.Guild.Role.t()}
+
+  @type guild_role_delete ::
+          {guild_id :: integer(), role :: Nostrum.Struct.Guild.Role.t()}
+
+  # Reaction events
+  @type message_reaction_add :: Nostrum.Struct.Event.MessageReactionAdd.t()
+
+  @type message_reaction_remove :: Nostrum.Struct.Event.MessageReactionRemove.t()
+
+  @type message_reaction_remove_all :: Nostrum.Struct.Event.MessageReactionRemoveAll.t()
+
+  @type message_reaction_remove_emoji :: Nostrum.Struct.Event.MessageReactionRemoveEmoji.t()
+
+  # Voice events
+  @type voice_state_update :: Nostrum.Struct.Event.VoiceState.t()
+
+  @type voice_ready :: Nostrum.Struct.Event.VoiceReady.t()
+
+  @type voice_server_update :: Nostrum.Struct.Event.VoiceServerUpdate.t()
+
+  @type voice_speaking_update :: Nostrum.Struct.Event.SpeakingUpdate.t()
+
+  @type voice_incoming_packet :: binary()
+
+  # User events
+  @type user_update ::
+          {old_user :: Nostrum.Struct.User.t() | nil, new_user :: Nostrum.Struct.User.t()}
+
+  # Presence events
+  @type presence_update ::
+          {guild_id :: integer(), old_presence :: map() | nil, new_presence :: map()}
+
+  # Emoji/Sticker events
+  @type guild_emojis_update ::
+          {guild_id :: integer(), old_emojis :: [Nostrum.Struct.Emoji.t()],
+           new_emojis :: [Nostrum.Struct.Emoji.t()]}
+
+  @type guild_stickers_update ::
+          {guild_id :: integer(), old_stickers :: [Nostrum.Struct.Sticker.t()],
+           new_stickers :: [Nostrum.Struct.Sticker.t()]}
+
+  # Thread events
+  @type thread_create :: Nostrum.Struct.Channel.t()
+
+  @type thread_update :: Nostrum.Struct.Channel.t()
+
+  @type thread_delete :: Nostrum.Struct.Channel.t() | :noop
+
+  @type thread_member_update :: Nostrum.Struct.ThreadMember.t()
+
+  @type thread_members_update :: Nostrum.Struct.Event.ThreadMembersUpdate.t()
+
+  @type thread_list_sync :: Nostrum.Struct.Event.ThreadListSync.t()
+
+  # Integration events
+  @type guild_integration_delete :: Nostrum.Struct.Event.GuildIntegrationDelete.t()
+
+  @type guild_integrations_update :: Nostrum.Struct.Event.GuildIntegrationsUpdate.t()
+
+  # Invite events
+  @type invite_create :: Nostrum.Struct.Event.InviteCreate.t()
+
+  @type invite_delete :: Nostrum.Struct.Event.InviteDelete.t()
+
+  # Ban events
+  @type guild_ban_add :: Nostrum.Struct.Event.GuildBanAdd.t()
+
+  @type guild_ban_remove :: Nostrum.Struct.Event.GuildBanRemove.t()
+
+  # AutoModeration events
+  @type auto_moderation_rule_create :: Nostrum.Struct.AutoModerationRule.t()
+
+  @type auto_moderation_rule_update :: Nostrum.Struct.AutoModerationRule.t()
+
+  @type auto_moderation_rule_delete :: Nostrum.Struct.AutoModerationRule.t()
+
+  @type auto_moderation_action_execution ::
+          Nostrum.Struct.Event.AutoModerationActionExecution.t()
+
+  # Audit log events
+  @type guild_audit_log_entry_create :: Nostrum.Struct.Event.GuildAuditLogEntryCreate.t()
+
+  # Other events
+  @type ready :: Nostrum.Struct.Event.Ready.t()
+
+  @type resumed :: map()
+
+  @type typing_start :: Nostrum.Struct.Event.TypingStart.t()
+
+  @type message_poll_vote_add :: Nostrum.Struct.Event.MessagePollVoteAdd.t()
+
+  @type message_poll_vote_remove :: Nostrum.Struct.Event.MessagePollVoteRemove.t()
+
+  @type webhooks_update :: map()
+
+  @type guild_members_chunk :: map()
+
+  # Union type of all payloads
   @type t ::
-          Nostrum.Struct.Message.t()
-          | {Nostrum.Struct.Message.t() | nil, Nostrum.Struct.Message.t()}
-          | Nostrum.Struct.Event.MessageDelete.t()
-          | Nostrum.Struct.Event.MessageDeleteBulk.t()
-
-          # Interaction events
-          | Nostrum.Struct.Interaction.t()
-
-          # Guild events
-          | Nostrum.Struct.Guild.t()
-          | {Nostrum.Struct.Guild.t(), Nostrum.Struct.Guild.t()}
-          | {Nostrum.Struct.Guild.t(), unavailable :: boolean()}
-
-          # Guild member events (tuple payloads)
-          | {guild_id :: integer(), Nostrum.Struct.Guild.Member.t()}
-          | {guild_id :: integer(), Nostrum.Struct.Guild.Member.t() | nil,
-             Nostrum.Struct.Guild.Member.t()}
-          | {guild_id :: integer(), user :: Nostrum.Struct.User.t()}
-
-          # Channel events
-          | Nostrum.Struct.Channel.t()
-          | {Nostrum.Struct.Channel.t() | nil, Nostrum.Struct.Channel.t()}
-          | Nostrum.Struct.Event.ChannelPinsUpdate.t()
-
-          # Role events (tuple payloads)
-          | {guild_id :: integer(), Nostrum.Struct.Guild.Role.t()}
-          | {guild_id :: integer(), Nostrum.Struct.Guild.Role.t() | nil,
-             Nostrum.Struct.Guild.Role.t()}
-
-          # Reaction events
-          | Nostrum.Struct.Event.MessageReactionAdd.t()
-          | Nostrum.Struct.Event.MessageReactionRemove.t()
-          | Nostrum.Struct.Event.MessageReactionRemoveAll.t()
-          | Nostrum.Struct.Event.MessageReactionRemoveEmoji.t()
-
-          # Voice events
-          | Nostrum.Struct.Event.VoiceState.t()
-          | Nostrum.Struct.Event.VoiceReady.t()
-          | Nostrum.Struct.Event.VoiceServerUpdate.t()
-          | Nostrum.Struct.Event.SpeakingUpdate.t()
-          | binary()
-
-          # User events
-          | {Nostrum.Struct.User.t() | nil, Nostrum.Struct.User.t()}
-
-          # Presence events
-          | {guild_id :: integer(), old_presence :: map() | nil, new_presence :: map()}
-
-          # Emoji/Sticker events (tuple payloads)
-          | {guild_id :: integer(), old_emojis :: [Nostrum.Struct.Emoji.t()],
-             new_emojis :: [Nostrum.Struct.Emoji.t()]}
-          | {guild_id :: integer(), old_stickers :: [Nostrum.Struct.Sticker.t()],
-             new_stickers :: [Nostrum.Struct.Sticker.t()]}
-
-          # Thread events
-          | Nostrum.Struct.Channel.t()
-          | Nostrum.Struct.ThreadMember.t()
-          | Nostrum.Struct.Event.ThreadMembersUpdate.t()
-          | Nostrum.Struct.Event.ThreadListSync.t()
-
-          # Integration events
-          | Nostrum.Struct.Event.GuildIntegrationDelete.t()
-          | Nostrum.Struct.Event.GuildIntegrationsUpdate.t()
-
-          # Invite events
-          | Nostrum.Struct.Event.InviteCreate.t()
-          | Nostrum.Struct.Event.InviteDelete.t()
-
-          # Ban events
-          | Nostrum.Struct.Event.GuildBanAdd.t()
-          | Nostrum.Struct.Event.GuildBanRemove.t()
-
-          # AutoModeration events
-          | Nostrum.Struct.AutoModerationRule.t()
-          | Nostrum.Struct.Event.AutoModerationActionExecution.t()
-
-          # Audit log events
-          | Nostrum.Struct.Event.GuildAuditLogEntryCreate.t()
-
-          # Other events
-          | Nostrum.Struct.Event.Ready.t()
-          | Nostrum.Struct.Event.TypingStart.t()
-          | Nostrum.Struct.Event.MessagePollVoteAdd.t()
-          | Nostrum.Struct.Event.MessagePollVoteRemove.t()
-
-          # Generic map payloads for events without specific structs
-          | map()
-
-          # Special cases
-          | :noop
+          message_create
+          | message_update
+          | message_delete
+          | message_delete_bulk
+          | interaction_create
+          | guild_create
+          | guild_update
+          | guild_delete
+          | guild_available
+          | guild_unavailable
+          | guild_member_add
+          | guild_member_update
+          | guild_member_remove
+          | channel_create
+          | channel_update
+          | channel_delete
+          | channel_pins_update
+          | guild_role_create
+          | guild_role_update
+          | guild_role_delete
+          | message_reaction_add
+          | message_reaction_remove
+          | message_reaction_remove_all
+          | message_reaction_remove_emoji
+          | voice_state_update
+          | voice_ready
+          | voice_server_update
+          | voice_speaking_update
+          | voice_incoming_packet
+          | user_update
+          | presence_update
+          | guild_emojis_update
+          | guild_stickers_update
+          | thread_create
+          | thread_update
+          | thread_delete
+          | thread_member_update
+          | thread_members_update
+          | thread_list_sync
+          | guild_integration_delete
+          | guild_integrations_update
+          | invite_create
+          | invite_delete
+          | guild_ban_add
+          | guild_ban_remove
+          | auto_moderation_rule_create
+          | auto_moderation_rule_update
+          | auto_moderation_rule_delete
+          | auto_moderation_action_execution
+          | guild_audit_log_entry_create
+          | ready
+          | resumed
+          | typing_start
+          | message_poll_vote_add
+          | message_poll_vote_remove
+          | webhooks_update
+          | guild_members_chunk
 end

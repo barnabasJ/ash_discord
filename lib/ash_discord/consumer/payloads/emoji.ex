096290bb592c21f188301661ce7dad31fd0d1548
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2025-10-04 00:45:45 +0200

    feat: expand Emoji TypedStruct with all Nostrum fields
    Add all 7 fields from Nostrum.Struct.Emoji with proper Ash types
    and custom new/1 function to convert from Nostrum structs.

--- lib/ash_discord/consumer/payloads/emoji.ex
+++ lib/ash_discord/consumer/payloads/emoji.ex
@@ -2,14 +2,27 @@ defmodule AshDiscord.Consumer.Payloads.Emoji do
   @moduledoc """
   TypedStruct wrapper for Discord Emoji data.
 
-  Wraps `Nostrum.Struct.Emoji.t()` to provide a unified AshDiscord type.
+  Provides a unified AshDiscord type with all fields from `Nostrum.Struct.Emoji.t()`.
   """
 
   use Ash.TypedStruct
 
   typed_struct do
-    field :data, :map,
-      allow_nil?: false,
-      description: "The Nostrum.Struct.Emoji struct"
+    field :id, :integer, description: "Id of the emoji"
+    field :name, :string, allow_nil?: false, description: "Name of the emoji"
+    field :roles, {:array, :integer}, description: "Roles this emoji is whitelisted to"
+    field :user, :map, description: "User that created this emoji"
+    field :require_colons, :boolean, description: "Whether this emoji must be wrapped in colons"
+    field :managed, :boolean, description: "Whether this emoji is managed"
+    field :animated, :boolean, description: "Whether this emoji is animated"
+  end
+
+  @doc """
+  Create an Emoji TypedStruct from a Nostrum Emoji struct.
+
+  Accepts a `Nostrum.Struct.Emoji.t()` and creates an AshDiscord Emoji TypedStruct.
+  """
+  def new(%Nostrum.Struct.Emoji{} = nostrum_emoji) do
+    super(Map.from_struct(nostrum_emoji))
   end
 end

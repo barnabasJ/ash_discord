901abc1b7c211c8b7d2fe1cce39bb26f26523775
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2025-10-08 22:12:31 +0200

    refactor(resumed handler): migrate to invoke_configured_action pattern

--- lib/ash_discord/consumer/handler/resumed.ex
+++ lib/ash_discord/consumer/handler/resumed.ex
@@ -1,9 +1,21 @@
 defmodule AshDiscord.Consumer.Handler.Resumed do
+  alias AshDiscord.Consumer.Handler
+
   @spec resumed(
           consumer :: module(),
           data :: map(),
           ws_state :: Nostrum.Struct.WSState.t(),
           context :: AshDiscord.Context.t()
         ) :: :ok | {:error, term()}
-  def resumed(_consumer, _data, _ws_state, _context), do: :ok
+  def resumed(_consumer, data, _ws_state, context) do
+    case Handler.invoke_configured_action(
+           :RESUMED,
+           %{},
+           %{data: data},
+           context
+         ) do
+      {:ok, _} -> :ok
+      {:error, error} -> {:error, error}
+    end
+  end
 end

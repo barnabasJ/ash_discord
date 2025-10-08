15bc950e13c5f48776c41b1bcdaaa230fe847f93
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2025-10-08 22:11:46 +0200

    refactor(presence handler): migrate to invoke_configured_action pattern

--- lib/ash_discord/consumer/handler/presence.ex
+++ lib/ash_discord/consumer/handler/presence.ex
@@ -1,10 +1,20 @@
 defmodule AshDiscord.Consumer.Handler.Presence do
+  alias AshDiscord.Consumer.Handler
+
   @spec update(
           new_presence :: AshDiscord.Consumer.Payload.presence_update(),
           ws_state :: Nostrum.Struct.WSState.t(),
           context :: AshDiscord.Context.t()
         ) :: :ok | {:error, term()}
-  def update({_guild_id, _old_presence, _new_presence}, _ws_state, _context) do
-    :ok
+  def update({guild_id, old_presence, new_presence}, _ws_state, context) do
+    case Handler.invoke_configured_action(
+           :PRESENCE_UPDATE,
+           %{user_discord_id: new_presence.user.id, guild_discord_id: guild_id},
+           %{guild_id: guild_id, old_presence: old_presence, new_presence: new_presence},
+           context
+         ) do
+      {:ok, _} -> :ok
+      {:error, error} -> {:error, error}
+    end
   end
 end

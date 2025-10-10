f3213bde2352f7270a9443fa9d4a6601cd7d6d28
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2025-10-10 22:24:43 +0200

    refactor: update Message resource attribute naming
    Rename channel_id, author_id, guild_id to channel_discord_id,
    author_discord_id, guild_discord_id for consistency. Update
    relationships to use new source attributes. Simplify create action by
    removing custom argument handling. Update identity name from
    unique_discord_id to discord_id.

--- test/support/test_app/discord/message.ex
+++ test/support/test_app/discord/message.ex
@@ -21,20 +21,20 @@ defmodule TestApp.Discord.Message do
 
     attribute(:discord_id, :integer, allow_nil?: false, public?: true)
     attribute(:content, :string, allow_nil?: true, public?: true)
-    attribute(:channel_id, :integer, public?: true)
-    attribute(:author_id, :integer, public?: true)
-    attribute(:guild_id, :integer, public?: true)
     attribute(:timestamp, :utc_datetime, public?: true)
     attribute(:edited_timestamp, :utc_datetime, public?: true)
     attribute(:tts, :boolean, public?: true, default: false)
     attribute(:mention_everyone, :boolean, public?: true, default: false)
     attribute(:pinned, :boolean, public?: true, default: false)
+    attribute(:channel_discord_id, :integer, public?: true)
+    attribute(:author_discord_id, :integer, public?: true)
+    attribute(:guild_discord_id, :integer, public?: true)
 
     timestamps()
   end
 
   identities do
-    identity(:unique_discord_id, [:discord_id], pre_check_with: TestApp.Discord)
+    identity(:discord_id, [:discord_id], pre_check_with: TestApp.Discord)
   end
 
   actions do
@@ -42,38 +42,18 @@ defmodule TestApp.Discord.Message do
 
     create :create do
       primary?(true)
-      accept([:discord_id, :content, :channel_id, :author_id, :guild_id])
-
-      argument(:message, :string)
-      argument(:channel, :string)
-
-      change(fn changeset, _context ->
-        message = Ash.Changeset.get_argument(changeset, :message)
-        channel = Ash.Changeset.get_argument(changeset, :channel)
-
-        changeset =
-          if message,
-            do: Ash.Changeset.change_attribute(changeset, :content, message),
-            else: changeset
-
-        changeset =
-          if channel,
-            do: Ash.Changeset.change_attribute(changeset, :channel_id, channel),
-            else: changeset
-
-        changeset
-      end)
+      accept([:discord_id, :content, :channel_discord_id, :author_discord_id, :guild_discord_id])
     end
 
     create :from_discord do
       upsert?(true)
-      upsert_identity(:unique_discord_id)
+      upsert_identity(:discord_id)
 
       upsert_fields([
         :content,
-        :channel_id,
-        :author_id,
-        :guild_id,
+        :channel_discord_id,
+        :author_discord_id,
+        :guild_discord_id,
         :timestamp,
         :edited_timestamp,
         :tts,
@@ -113,8 +93,8 @@ defmodule TestApp.Discord.Message do
         changeset
         |> Ash.Changeset.change_attribute(:content, "Hello from AshDiscord!")
         |> Ash.Changeset.change_attribute(:discord_id, System.system_time(:nanosecond))
-        |> Ash.Changeset.change_attribute(:channel_id, 123_456_789)
-        |> Ash.Changeset.change_attribute(:author_id, 987_654_321)
+        |> Ash.Changeset.change_attribute(:channel_discord_id, 123_456_789)
+        |> Ash.Changeset.change_attribute(:author_discord_id, 987_654_321)
       end)
     end
   end
@@ -122,17 +102,17 @@ defmodule TestApp.Discord.Message do
   relationships do
     belongs_to(:guild, TestApp.Discord.Guild,
       destination_attribute: :discord_id,
-      source_attribute: :guild_id
+      source_attribute: :guild_discord_id
     )
 
     belongs_to(:author, TestApp.Discord.User,
       destination_attribute: :discord_id,
-      source_attribute: :author_id
+      source_attribute: :author_discord_id
     )
 
     belongs_to(:channel, TestApp.Discord.Channel,
       destination_attribute: :discord_id,
-      source_attribute: :channel_id
+      source_attribute: :channel_discord_id
     )
   end
 

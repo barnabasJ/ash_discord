16bc9ebe574f130c7376bc19a9f1570fbaf23a07
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2025-10-10 19:55:43 +0200

    test: add ash_discord context marker to handler tests
    Add ash_discord?: true markers to context in ready and resumed handler
    tests to indicate actions are invoked from AshDiscord event handlers,
    allowing authorization policies to correctly identify Discord-originated
    requests.

--- test/ash_discord/consumer/handler/ready_test.exs
+++ test/ash_discord/consumer/handler/ready_test.exs
@@ -19,7 +19,11 @@ defmodule AshDiscord.Consumer.Handler.ReadyTest do
         consumer: TestConsumer,
         resource: TestApp.Discord.Event,
         guild: nil,
-        user: nil
+        user: nil,
+        context: %{
+          private: %{ash_discord?: true},
+          shared: %{private: %{ash_discord?: true}}
+        }
       }
 
       log =
--- test/ash_discord/consumer/handler/resumed_test.exs
+++ test/ash_discord/consumer/handler/resumed_test.exs
@@ -20,7 +20,11 @@ defmodule AshDiscord.Consumer.Handler.ResumedTest do
         consumer: TestConsumer,
         resource: TestApp.Discord.Event,
         guild: nil,
-        user: nil
+        user: nil,
+        context: %{
+          private: %{ash_discord?: true},
+          shared: %{private: %{ash_discord?: true}}
+        }
       }
 
       log =

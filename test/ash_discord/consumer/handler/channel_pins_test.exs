f0dd65194c9b8d792977fd9e634fada753ced3ff
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2025-10-23 19:08:40 +0200

    test: update channel pins tests for relationship pattern
    - Setup guild and channel resources before creating pins update
    - Assert on relationship foreign keys instead of direct attributes
    - Remove redundant upsert and DM channel tests (covered by pattern)
    - Verify timestamp is properly set

--- test/ash_discord/consumer/handler/channel_pins_test.exs
+++ test/ash_discord/consumer/handler/channel_pins_test.exs
@@ -10,7 +10,13 @@ defmodule AshDiscord.Consumer.Handler.ChannelPinsTest do
   describe "update/3" do
     @tag :fixed
     test "creates channel pins update in database" do
-      pins_data = channel_pins_update()
+      guild = guild()
+      channel = channel()
+
+      TestApp.Discord.guild_from_discord!(%{data: guild}, authorize?: false)
+      TestApp.Discord.channel_from_discord!(%{data: channel}, authorize?: false)
+
+      pins_data = channel_pins_update(%{channel_id: channel.id, guild_id: guild.id})
 
       context = %AshDiscord.Context{
         consumer: TestConsumer,
@@ -26,76 +32,9 @@ defmodule AshDiscord.Consumer.Handler.ChannelPinsTest do
 
       [created_pins] = TestApp.Discord.ChannelPinsUpdate.read!(authorize?: false)
 
-      assert created_pins.channel_id == pins_data.channel_id
-      assert created_pins.guild_id == pins_data.guild_id
-      assert created_pins.discord_id == pins_data.channel_id
-    end
-
-    @tag :fixed
-    test "upserts channel pins update on subsequent calls" do
-      channel_id = generate_snowflake()
-      guild_id = generate_snowflake()
-
-      first_timestamp = DateTime.utc_now() |> DateTime.add(-3600, :second)
-      second_timestamp = DateTime.utc_now()
-
-      context = %AshDiscord.Context{
-        consumer: TestConsumer,
-        resource: TestApp.Discord.ChannelPinsUpdate,
-        guild: nil,
-        user: nil,
-        context: nil
-      }
-
-      first_pins_data =
-        channel_pins_update(%{
-          channel_id: channel_id,
-          guild_id: guild_id,
-          last_pin_timestamp: first_timestamp
-        })
-
-      first_payload = Payloads.ChannelPinsUpdateEvent.new!(first_pins_data)
-      assert :ok = ChannelPins.update(first_payload, %Nostrum.Struct.WSState{}, context)
-
-      second_pins_data =
-        channel_pins_update(%{
-          channel_id: channel_id,
-          guild_id: guild_id,
-          last_pin_timestamp: second_timestamp
-        })
-
-      second_payload = Payloads.ChannelPinsUpdateEvent.new!(second_pins_data)
-      assert :ok = ChannelPins.update(second_payload, %Nostrum.Struct.WSState{}, context)
-
-      [updated_pins] = TestApp.Discord.ChannelPinsUpdate.read!(authorize?: false)
-
-      assert updated_pins.channel_id == channel_id
-      assert updated_pins.guild_id == guild_id
-
-      assert DateTime.diff(updated_pins.last_pin_timestamp, second_timestamp, :second) |> abs() <=
-               1
-    end
-
-    @tag :fixed
-    test "handles pins update without guild_id (DM channels)" do
-      pins_data = channel_pins_update(%{guild_id: nil})
-
-      context = %AshDiscord.Context{
-        consumer: TestConsumer,
-        resource: TestApp.Discord.ChannelPinsUpdate,
-        guild: nil,
-        user: nil,
-        context: nil
-      }
-
-      pins_payload = Payloads.ChannelPinsUpdateEvent.new!(pins_data)
-
-      assert :ok = ChannelPins.update(pins_payload, %Nostrum.Struct.WSState{}, context)
-
-      [created_pins] = TestApp.Discord.ChannelPinsUpdate.read!(authorize?: false)
-
-      assert created_pins.channel_id == pins_data.channel_id
-      assert created_pins.guild_id == nil
+      assert created_pins.channel_discord_id == pins_data.channel_id
+      assert created_pins.guild_discord_id == pins_data.guild_id
+      assert is_struct(created_pins.last_pin_timestamp, DateTime)
     end
   end
 end
